08cf1616b1a180e38fb2a3f44694783c393ab2b3
JD Porterfield <jdrocks94@gmail.com>
2021-01-17 17:35:46 -0500

    No Ticket : Remove Kotlin Synthetics
    feat : Kotlin synthetics have been deprecated in Kotlin
    1.4.20. As such they have been removed. So that I don't
    need to worry about deprecations of other tools for a simple
    example project (ViewBinding, Butterknife, etc.), I've
    decided to just do manual lookups of the views using
    findViewById

--- app/src/main/java/me/jdvp/androidaspectexample/activity/MainActivity.kt
+++ app/src/main/java/me/jdvp/androidaspectexample/activity/MainActivity.kt
@@ -2,15 +2,19 @@ package me.jdvp.androidaspectexample.activity
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import me.jdvp.androidaspectexample.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Using findViewById so we don't have to worry about deprecations later
+        val button1 = findViewById<Button>(R.id.button_1)
+        val button2 = findViewById<Button>(R.id.button_2)
 
         //The button ClickListener's below only have actions that create Toasts.
         //However, when you press the buttons at runtime, you will notice in LogCat
@@ -20,13 +24,13 @@ class MainActivity : AppCompatActivity() {
 
         //Not a lambda on purpose for demonstration purposes
         @Suppress("ObjectLiteralToLambda")
-        button_1.setOnClickListener(object : View.OnClickListener {
+        button1.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 Toast.makeText(this@MainActivity, "Button 1 clicked", Toast.LENGTH_SHORT).show()
             }
         })
 
-        button_2.setOnClickListener {
+        button2.setOnClickListener {
             Toast.makeText(this@MainActivity, "Button 2 clicked", Toast.LENGTH_SHORT).show()
         }
     }
